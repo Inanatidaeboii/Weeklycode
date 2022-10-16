559add953395e79e97b20c62d9ddb02900c64754
Inanatidaeboii <109221616+Inanatidaeboii@users.noreply.github.com>
2022-10-16 23:17:07 +0700

    Add files via upload

---
+++ week3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+    int x,y,sums;
+    printf("enter your nums: ");
+    scanf("%d %d",&x,&y);
+    sums = x+y ;
+    printf("%d + %d = %d",x,y,sums);
+    return 0;
+}
